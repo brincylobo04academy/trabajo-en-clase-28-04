9c23bdee298f7eec6fbd724f2d0cb09cc31988ca
brincylobo04academy <brincy.lobo.monestel@academiacuatropuntocero.com>
2022-04-29 14:15:50 -0600

    mi trabajo en clase 28-04

---
+++ Trabajo en clase JS.js
@@ -0,0 +1,47 @@
+// Trabajo en clase
+
+function comprar_una_plancha (_esta_barata) {
+    let recomendacion_mama = cara(vamos_a_ver)
+    if (recomendacion_mama <70){
+        console.log('siuuu me la compro');
+    }else{
+        console.log('shhh no la compro')
+    }
+}
+
+console.log('siuuu me la compro');
+
+
+function say_hola (){
+     console.log ('Vea profe, estoy mejorando');
+}
+
+say_hola();
+
+
+function suma_de_three_numeros (numero1, numero2, numero3){
+    console.log(numero1+numero2+numero3);
+}
+suma_de_three_numeros(23,7,35);
+
+
+
+
+ var me_invitaron = false;
+function voy_a_la_fiesta (_fiesta){
+    if (me_invitaron == false){
+        console.log('yupii me invitaron');
+    }else if (me_invitaron == true){
+        console.log('que sad no me invitaron');
+    }
+}
+
+voy_a_la_fiesta(false);
+
+
+function multiplica_de_tres_numeros (numero1, numero2, numero3){
+    console.log(numero1 * numero2 * numero3);
+}
+
+
+multiplica_de_tres_numeros(8,2,4);
